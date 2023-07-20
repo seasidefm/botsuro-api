1c1c4dbd3ff907e72de32a0e5d9be38deb2215b5
Duke_Ferdinand <doug@dougflynn.dev>
2023-07-20 14:29:13 -0700

    Update weather to exclude data to fit in 8k token limit

--- services/OpenWeather.py
+++ services/OpenWeather.py
@@ -2,14 +2,22 @@ from typing import Union
 
 import requests
 
-from models.weather import WeatherModel, WeatherData
-
-
 class OpenWeatherApi:
+    """
+    API wrapper for connecting to OpenWeather
+    """
     def __init__(self, api_key: str):
-        self.base_url = f"https://api.openweathermap.org/data/3.0/onecall?lat=LATITUDE&lon=LONGITUDE&appid={api_key}"
+        self.base_url = f"https://api.openweathermap.org/data/3.0/onecall?lat=LATITUDE&lon=LONGITUDE" \
+            "&appid={api_key}&exclude=minutely,alerts"
 
     def get_weather_for_coords(self, latitude: Union[str, float], longitude: Union[str, float]):
+        """
+        Get the weather for a given set of coordinates
+        :param latitude:
+        :param longitude:
+        :return:
+        """
+
         formatted_url = self.base_url.replace('LATITUDE', str(latitude)).replace('LONGITUDE', str(longitude))
 
         data = requests.get(formatted_url)
