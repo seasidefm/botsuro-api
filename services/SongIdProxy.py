d3767d115680317f4e8b183ff1a03bbe2bf62f55
Duke_Ferdinand <doug@dougflynn.dev>
2023-07-23 18:00:26 -0700

    Catch connection error for song health check

--- services/SongIdProxy.py
+++ services/SongIdProxy.py
@@ -2,11 +2,26 @@ import requests
 
 
 class SongIdProxy:
+    """
+    API wrapper for connecting to Song ID
+    """
     def __init__(self, endpoint: str):
         self.endpoint = endpoint
 
     def health_check(self):
-        return requests.get(self.endpoint, timeout=45).status_code == 200, "OK"
+        """
+        Check if Song ID API is doing okay
+        :return:
+        """
+        try:
+            return requests.get(self.endpoint, timeout=45).status_code == 200, "OK"
+        except ConnectionError:
+            return False, "Could not connect to Song ID API"
 
     def get_song_id(self, creator: str):
+        """
+        Get the song ID for a given creator
+        :param creator:
+        :return:
+        """
         return requests.get(f"{self.endpoint}/identify/{creator}", timeout=90).json()
