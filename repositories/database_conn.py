27d47fa101bb6a16774885c77af34baec905b5a4
Doug <doug@dougflynn.dev>
2023-08-24 13:53:50 -0700

    use certifi for root TLS certs

--- repositories/database_conn.py
+++ repositories/database_conn.py
@@ -2,13 +2,14 @@ import os
 from typing import Collection, Mapping, Any
 from urllib.parse import urlparse
 
+import certifi
 from pymongo import MongoClient
 
 
 class DatabaseConn:
     def __init__(self):
         if mongo_url := os.getenv("DATABASE_URL"):
-            self.db_instance = MongoClient(mongo_url, connect=True)
+            self.db_instance = MongoClient(mongo_url, connect=True, tlsCAFile=certifi.where())
             self.selected_db = self._get_db_from_url(mongo_url=mongo_url)
         else:
             raise EnvironmentError("DATABASE_URL not found in env")
