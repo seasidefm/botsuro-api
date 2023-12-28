df5fd347f231ec99809d4d782909d40879edb815
Doug <doug@dougflynn.dev>
2023-12-28 01:03:01 -0800

    add a dotenv load in streams service

--- services/streams/__init__.py
+++ services/streams/__init__.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
+
 from repositories.xiu_server import XiuServer
 
 
 class StreamsService:
     def __init__(self):
+        load_dotenv()
         self.xiu_service = XiuServer()
 
     def get_active_streams(self):
